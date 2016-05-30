696601f482150f23c60719c791d2510aa5a97031
karbfg10k <karthik.natraj@gmail.com>
2016-05-30 15:33:07 -0400

    Intial commit

---
+++ Python/fastprime.py
@@ -0,0 +1,19 @@
+import time
+
+
+
+
+
+def isPrime(num, prime_list):
+    prev = 0
+    if prime_list:
+        for prime_no in prim_list:
+            if num % prime_no == 0:
+                return false
+                prev = prime_no
+            if x / prev > 
+    return true
+
+start = time.clock()
+p_list = [2]
+
