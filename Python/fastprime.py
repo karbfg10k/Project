0fafaf21c030d6969c33f1fd72bb339662afedf2
karbfg10k <karthik.natraj@gmail.com>
2016-05-30 15:46:24 -0400

    First rev of fastprime

--- Python/fastprime.py
+++ Python/fastprime.py
@@ -1,19 +1,27 @@
 import time
 
-
-
-
-
 def isPrime(num, prime_list):
     prev = 0
     if prime_list:
-        for prime_no in prim_list:
+        for prime_no in prime_list:
             if num % prime_no == 0:
-                return false
+                return False
                 prev = prime_no
-            if x / prev > 
-    return true
+            if num / prime_no < prev:
+                break
+    return True
 
 start = time.clock()
-p_list = [2]
+p_list = []
+max = 100000
+i = 2
+while True:
+    if isPrime(i, p_list) == True:
+        p_list.append(i)
+    if i == max:
+        break
+    i+=1
 
+print("Done: ", p_list[-1])
+print("List: ", len(p_list))
+print("Time: ", time.clock() - start)
